10454090e7951927ef1629d3d4f9a00a5db9c5a5
gigihpaga <gigihpaga@gmail.com>
2023-08-03 18:47:12 +0700

    [Mod_ Role] - membuat view role-form, digunakan untuk tambah data dan update data

---
+++ resources/views/pages/role/role-form.blade.php
@@ -0,0 +1,35 @@
+<div class="modal-content">
+    <form id="form-action" action="{{ route('roles.update', $role->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="modal-header">
+            <h5 class="modal-title" id="largeModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="roleName" class="form-label">Name</label>
+                        <input type="text" value="{{ $role->name }}" placeholder="Role name" name="name"
+                            class="form-control" id="roleName" required>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="guardName" class="form-label">Guard</label>
+                        <input type="text" value="{{ $role->guard_name }}" placeholder="Guard name" name="guard_name"
+                            class="form-control" id="guardName" required>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top"
+                title="Save Change" data-bs-original-title="Save Change">
+                <i class="ti-save"></i>
+            </button>
+        </div>
+    </form>
+</div>
