02af7d1815e08aa529e841956616364bed8a4e03
gigihpaga <gigihpaga@gmail.com>
2023-08-09 02:33:29 +0700

    [Mod_ User] - menambah relasi ke table bagian

---
+++ database/migrations/2023_08_08_185858_add_bagian_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->foreignId('bagian_id')->after('username');
+
+            $table->foreign('bagian_id')->references('id')->on('bagian')->onUpdate('cascade');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
