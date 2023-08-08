2f2065cb93855f279c9f267fb2493b15a646fdd0
gigihpaga <gigihpaga@gmail.com>
2023-08-08 12:57:59 +0700

    [Mod_ PKA SPT] - model spt, menambah relasi ke pka, user, statusHistorys

--- app/Models/Spt.php
+++ app/Models/Spt.php
@@ -11,4 +11,29 @@ class Spt extends Model
 
     protected $table = 'spt';
     protected $guarded = ['id'];
+
+    // join ke pka
+    public function pka()
+    {
+        return $this->hasOne(Pka::class, 'id', 'pka_id');
+    }
+
+    // join ke table user
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'pemohon_spt');
+    }
+
+    // join ke table spt_status_history
+    public function statusHistorys()
+    {
+        return $this->hasMany(SptStatusHistory::class);
+    }
+
+    // join ke table spt_status_history, dan ambil id terbesar (paling akhir)
+    public function lastStatusHistory()
+    {
+        // ambil id terbesar dari status history
+        return $this->hasOne(SptStatusHistory::class)->latestOfMany();
+    }
 }
