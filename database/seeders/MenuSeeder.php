3854772ff6e36ffa5d9a15de7de75521f602d35a
gigihpaga <gigihpaga@gmail.com>
2023-08-09 02:34:56 +0700

    [Mod_ Menu] - upadte menu dan role permision seeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -40,5 +40,61 @@ class MenuSeeder extends Seeder
             'url' => 'konfigurasi/permissions',
             'icon' => '',
         ]);
+        Menu::create([
+            'sort' => 4,
+            'display_name' => 'Dokumen',
+            'type' => 'main_menu',
+            'parrent_menu_id' => null,
+            'url' => 'dokumen',
+            'icon' => 'ti-files',
+        ]);
+        Menu::create([
+            'sort' => 5,
+            'display_name' => 'PKA dan SPT',
+            'type' => 'sub_menu',
+            'parrent_menu_id' => 4,
+            'url' => 'dokumen/pkaspt',
+            'icon' => '',
+        ]);
+        Menu::create([
+            'sort' => 6,
+            'display_name' => 'Master',
+            'type' => 'main_menu',
+            'parrent_menu_id' => null,
+            'url' => 'master',
+            'icon' => ' ti-folder',
+        ]);
+        Menu::create([
+            'sort' => 7,
+            'display_name' => 'Jabatan',
+            'type' => 'sub_menu',
+            'parrent_menu_id' => 6,
+            'url' => 'master/jabatan',
+            'icon' => '',
+        ]);
+        Menu::create([
+            'sort' => 8,
+            'display_name' => 'Pangkat Golongan',
+            'type' => 'sub_menu',
+            'parrent_menu_id' => 6,
+            'url' => 'master/pangkat_golongan',
+            'icon' => '',
+        ]);
+        Menu::create([
+            'sort' => 9,
+            'display_name' => 'Kelas Perjadin',
+            'type' => 'sub_menu',
+            'parrent_menu_id' => 6,
+            'url' => 'master/kelas_perjadin',
+            'icon' => '',
+        ]);
+        Menu::create([
+            'sort' => 10,
+            'display_name' => 'Pegawai',
+            'type' => 'sub_menu',
+            'parrent_menu_id' => 6,
+            'url' => 'master/pegawai',
+            'icon' => '',
+        ]);
     }
 }
--- database/seeders/UserRolePermisionSeeder.php
+++ database/seeders/UserRolePermisionSeeder.php
@@ -33,6 +33,7 @@ class UserRolePermisionSeeder extends Seeder
                 [
                     'name' => 'Paga Admin',
                     'username' => 'paga',
+                    'bagian_id' => 1,
                     'email' => 'useradmin@mail.com',
                 ],
                 $default_value
@@ -41,6 +42,7 @@ class UserRolePermisionSeeder extends Seeder
                 [
                     'name' => 'Ido Manager',
                     'username' => 'ido',
+                    'bagian_id' => 1,
                     'email' => 'usermanager@mail.com',
                 ],
                 $default_value
@@ -49,6 +51,7 @@ class UserRolePermisionSeeder extends Seeder
                 [
                     'name' => 'Dani Spv',
                     'username' => 'dani',
+                    'bagian_id' => 1,
                     'email' => 'userspv@mail.com',
                 ],
                 $default_value
@@ -57,6 +60,7 @@ class UserRolePermisionSeeder extends Seeder
                 [
                     'name' => 'Tika Staff',
                     'username' => 'tika',
+                    'bagian_id' => 1,
                     'email' => 'userstaff@mail.com',
                 ],
                 $default_value
@@ -65,6 +69,7 @@ class UserRolePermisionSeeder extends Seeder
                 [
                     'name' => 'Dian Admin',
                     'username' => 'dian',
+                    'bagian_id' => 1,
                     'email' => 'dianadmin@mail.com',
                 ],
                 $default_value
@@ -87,6 +92,40 @@ class UserRolePermisionSeeder extends Seeder
             Permission::create(['name' => 'update_konfigurasi/permissions']); // 8
             Permission::create(['name' => 'delete_konfigurasi/permissions']); // 9
 
+            Permission::create(['name' => 'read_dokumen']); //
+            Permission::create(['name' => 'read_dokumen/pkaspt']); //
+            Permission::create(['name' => 'create_dokumen/pkaspt']); //
+            Permission::create(['name' => 'update_dokumen/pkaspt']); //
+            Permission::create(['name' => 'delete_dokumen/pkaspt']); //
+
+            // main menu : master
+            Permission::create(['name' => 'read_master']); //
+
+            // jabatan
+            Permission::create(['name' => 'read_master/jabatan']); //
+            Permission::create(['name' => 'create_master/jabatan']); //
+            Permission::create(['name' => 'update_master/jabatan']); //
+            Permission::create(['name' => 'delete_master/jabatan']); //
+
+            // pangkat golongan
+            Permission::create(['name' => 'read_master/pangkat_golongan']); //
+            Permission::create(['name' => 'create_master/pangkat_golongan']); //
+            Permission::create(['name' => 'update_master/pangkat_golongan']); //
+            Permission::create(['name' => 'delete_master/pangkat_golongan']); //
+
+            // kelas perjadin
+            Permission::create(['name' => 'read_master/kelas_perjadin']); //
+            Permission::create(['name' => 'create_master/kelas_perjadin']); //
+            Permission::create(['name' => 'update_master/kelas_perjadin']); //
+            Permission::create(['name' => 'delete_master/kelas_perjadin']); //
+
+            // pegawai
+            Permission::create(['name' => 'read_master/pegawai']); //
+            Permission::create(['name' => 'create_master/pegawai']); //
+            Permission::create(['name' => 'update_master/pegawai']); //
+            Permission::create(['name' => 'delete_master/pegawai']); //
+
+
             /**
              * memasangkan role admin ke permision,
              * data ini akan masuk table role_has_permissions
@@ -97,6 +136,43 @@ class UserRolePermisionSeeder extends Seeder
             $role_admin->givePermissionTo('create_konfigurasi/roles');
             $role_admin->givePermissionTo('update_konfigurasi/roles');
             $role_admin->givePermissionTo('delete_konfigurasi/roles');
+
+
+            // master submenu
+            $role_admin->givePermissionTo('read_master');
+
+            // jabatan
+            $role_admin->givePermissionTo('read_master/jabatan');
+            $role_admin->givePermissionTo('create_master/jabatan');
+            $role_admin->givePermissionTo('update_master/jabatan');
+            $role_admin->givePermissionTo('delete_master/jabatan');
+
+            // pangkat golongan
+            $role_admin->givePermissionTo('read_master/pangkat_golongan');
+            $role_admin->givePermissionTo('create_master/pangkat_golongan');
+            $role_admin->givePermissionTo('update_master/pangkat_golongan');
+            $role_admin->givePermissionTo('delete_master/pangkat_golongan');
+
+            // kelas perjadian
+            $role_admin->givePermissionTo('read_master/kelas_perjadin');
+            $role_admin->givePermissionTo('create_master/kelas_perjadin');
+            $role_admin->givePermissionTo('update_master/kelas_perjadin');
+            $role_admin->givePermissionTo('delete_master/kelas_perjadin');
+
+            // pegawai
+            $role_admin->givePermissionTo('read_master/pegawai');
+            $role_admin->givePermissionTo('create_master/pegawai');
+            $role_admin->givePermissionTo('update_master/pegawai');
+            $role_admin->givePermissionTo('delete_master/pegawai');
+
+            // pka spt
+            $role_admin->givePermissionTo([
+                'read_dokumen',
+                'read_dokumen/pkaspt',
+                'create_dokumen/pkaspt',
+                'update_dokumen/pkaspt',
+                'delete_dokumen/pkaspt',
+            ]);
 
             /**
              * memasangkan data user dengan data permision menggunakan relasi spatie
