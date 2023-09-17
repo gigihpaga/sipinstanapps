40f07e16b921d380bfe447fc5f85616d4cedd745
Gigih Patga <gigihpatga@gmail.com>
2023-09-18 04:17:12 +0700

    [Theming] - create component logo blade using svg #2 logo-sipinstan no-border

---
+++ resources/views/components/application-logo-sipinstan-no-border.blade.php
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-16"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<!-- Creator: CorelDRAW 2020 (64-Bit) -->
+<svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="446px" height="446px" version="1.1"
+    style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+    viewBox="0 0 446 446" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xodm="http://www.corel.com/coreldraw/odm/2003">
+    <defs>
+        <style type="text/css">
+            <![CDATA[
+            .fil0 {
+                fill: none
+            }
+
+            .fil6 {
+                fill: url(#id0)
+            }
+
+            .fil1 {
+                fill: url(#id1)
+            }
+
+            .fil3 {
+                fill: url(#id2)
+            }
+
+            .fil2 {
+                fill: url(#id3)
+            }
+
+            .fil4 {
+                fill: url(#id4)
+            }
+
+            .fil5 {
+                fill: url(#id5)
+            }
+            ]]>
+        </style>
+        <linearGradient id="id0" gradientUnits="userSpaceOnUse" x1="445.74" y1="122.35" x2="297.36"
+            y2="183.65">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1A98E2" />
+            <stop offset="0.25098" style="stop-opacity:1; stop-color:#1B7CCE" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1C60BA" />
+        </linearGradient>
+        <linearGradient id="id1" gradientUnits="userSpaceOnUse" x1="35.05" y1="259.49" x2="283.29"
+            y2="71.73">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1C60BA" />
+            <stop offset="0.678431" style="stop-opacity:1; stop-color:#1B7CCE" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1A98E2" />
+        </linearGradient>
+        <linearGradient id="id2" gradientUnits="userSpaceOnUse" x1="137.37" y1="274.34" x2="66"
+            y2="266.15">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1A98E2" />
+            <stop offset="0.388235" style="stop-opacity:1; stop-color:#1B7CCE" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1C60BA" />
+        </linearGradient>
+        <linearGradient id="id3" gradientUnits="userSpaceOnUse" x1="262.38" y1="77.57" x2="119.19"
+            y2="163.07">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1A98E2" />
+            <stop offset="0.509804" style="stop-opacity:1; stop-color:#1B80D1" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1C68C0" />
+        </linearGradient>
+        <linearGradient id="id4" gradientUnits="userSpaceOnUse" x1="104.29" y1="248.7" x2="278.85"
+            y2="127.18">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1C60BA" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1A98E2" />
+        </linearGradient>
+        <linearGradient id="id5" gradientUnits="userSpaceOnUse" x1="241.95" y1="139.21" x2="412.52"
+            y2="251.26">
+            <stop offset="0" style="stop-opacity:1; stop-color:#1A98E2" />
+            <stop offset="0.329412" style="stop-opacity:1; stop-color:#1B7CCE" />
+            <stop offset="1" style="stop-opacity:1; stop-color:#1C60BA" />
+        </linearGradient>
+    </defs>
+    <g id="Layer_x0020_1">
+        <metadata id="CorelCorpID_0Corel-Layer" />
+        <rect id="BASE_x0020_TRANSAPARAN" class="fil0" width="446" height="446" />
+        <g id="KEPAKE">
+            <g>
+                <path class="fil1"
+                    d="M301 48.37c-53,57.68 -81.57,135.44 -93.35,174.84 -11.78,39.39 -54.88,142.79 -131.93,138.27 -62.35,-3.66 -75.85,-58.74 -75.23,-84.46 0.95,-39.74 16.85,-71.94 56.52,-81.86 39.67,-9.92 61.5,1.98 61.5,1.98 0,0 41.7,-90.26 75.42,-127.94 33.72,-37.69 107.07,-20.83 107.07,-20.83z" />
+                <path class="fil2"
+                    d="M143.77 219.5c6.23,-27.93 26.78,-83.5 66.78,-113.5 11.14,-8.35 63.25,-45.72 76,-50l-156 278 13.22 -114.5z" />
+                <path class="fil3"
+                    d="M190.9 194.79c0,15.87 -7.69,68.97 -19.84,91.25 -18.51,33.96 -32.99,65.2 -64,71.14 -21.4,4.09 -64.37,1.68 -82.95,-17.49 -22.23,-22.94 -23.62,-62.67 -19.42,-92.89 11.86,-38.8 45.23,-51.48 57.79,-52.89 59.95,-6.77 12.56,148.97 53.07,134.09 49,-18 23.32,-46.57 40,-70 15.92,-22.37 21.76,-32.32 35.35,-63.21z" />
+                <path class="fil4"
+                    d="M277.71 76.95c-38.31,52.69 -60.14,113.08 -70.06,146.26 -11.78,39.39 -54.88,142.79 -131.93,138.27 -12.45,-0.73 -22.95,-3.52 -31.79,-7.71 4.26,-1.89 13.67,-0.71 21.01,-1.7 103.16,-13.88 73.97,-92.75 101.17,-154.72 27.21,-61.98 68.46,-87.41 111.6,-120.4z" />
+            </g>
+            <g>
+                <path class="fil5"
+                    d="M145 397.63c53,-57.68 81.57,-135.44 93.35,-174.84 11.78,-39.39 54.88,-142.79 131.93,-138.27 62.35,3.66 75.85,58.74 75.23,84.46 -0.95,39.74 -16.85,71.94 -56.52,81.86 -39.67,9.92 -61.49,-1.98 -61.49,-1.98 0,0 -41.71,90.26 -75.43,127.94 -33.72,37.69 -107.07,20.83 -107.07,20.83z" />
+                <path class="fil6"
+                    d="M294.7 119.1l0.05 -0.06 18.96 -16.86c16.26,-11.79 35.09,-18.91 56.57,-17.66 62.35,3.66 75.85,58.74 75.23,84.46 -0.52,21.73 -5.51,41.21 -16.42,56.03 8.51,-74.97 -54.55,-85.69 -86.7,-74.97 -65.47,21.82 -47.33,-30.84 -47.69,-30.94z" />
+            </g>
+        </g>
+    </g>
+</svg>
