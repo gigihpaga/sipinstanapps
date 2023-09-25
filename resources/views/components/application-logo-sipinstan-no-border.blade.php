78822734c84511f740918233a343e220f399afc5
Gigih Patga <gigihpatga@gmail.com>
2023-09-25 23:39:26 +0700

    [Theming] - logo blade svg #2 logo-sipinstan no-border, delete xml version

--- resources/views/components/application-logo-sipinstan-no-border.blade.php
+++ resources/views/components/application-logo-sipinstan-no-border.blade.php
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-16"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <!-- Creator: CorelDRAW 2020 (64-Bit) -->
 <svg {{ $attributes }} xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="446px" height="446px" version="1.1"
